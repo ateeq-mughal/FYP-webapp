58ff485ef3e4253c91e2c8c22954fa6145a11e61
AteeqUrRehman <ateeqmughal272@gmail.com>
2022-03-10 05:48:05 +0500

    minor changes

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mzv$lt&1g&pga2j1si+hkuf7ypw-peyqk7l8p_*%m-8bsa^)v*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['fydp-webapp.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -115,7 +115,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
